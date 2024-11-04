5928e183fc495d66c78c0bcec9d4901365e9000c
suhaasya <suhaskhobragade19@gmail.com>
2024-11-04 21:56:16 +0530

    adds env support for port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"todotwebp/internal/routes"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 )
 
@@ -21,7 +23,17 @@ func main() {
 		AllowCredentials: true,
 	})
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	// Wrap handler with CORS and start the server
-	fmt.Println("Running Server on port :8080")
-	log.Fatal(http.ListenAndServe(":8080", c.Handler(handler)))
+	fmt.Printf("Running Server on port :%v \n", port)
+	log.Fatal(http.ListenAndServe(":"+port, c.Handler(handler)))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module todotwebp
 go 1.22.3
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/kolesa-team/go-webp v1.0.4
 	github.com/rs/cors v1.11.1
 )
